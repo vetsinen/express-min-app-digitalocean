5b042bf14f8317a11544f18f5019b5cf7826cf49
vetsinen <vetsinen@gmail.com>
2021-02-25 15:49:07 +0200

    renders dynamic list

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const path = require("path");
+const nunjucks = require("nunjucks");
 const app = express()
 const port = 8080
 
+// set the view engine to ejs
+app.set('view engine', 'nunjucks');
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+app.use(express.static( path.join(__dirname, 'public')));
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    const data = ['fdfsdf','4fsdf44','vndssxgs','jxgwxhww']
+    res.render('index.html', {data:data});
+    // res.send('Hello World!')
 })
 
 app.listen(port, () => {
